9b4b176cdb84db5f0b348ff45e262942a6803c4e
SirShizu <maheshraturi11a@gmail.com>
2024-10-01 19:22:40 +0530

    git commit -m "new project started"

---
+++ src/main/java/com/mahesh/ToDo/List/WebApp/ToDoListWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.mahesh.ToDo.List.WebApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoListWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToDoListWebAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mahesh/ToDo/List/WebApp/ToDoListWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mahesh.ToDo.List.WebApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoListWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
